36092fc6a43e45e57932c06f5bda6aaca7e47ad0
Mateus Leonhardt <mateus.leonhardt@conectt.com.br>
2019-09-03 10:28:18 -0300

    App finished

--- instarocketv2/src/pages/New.js
+++ instarocketv2/src/pages/New.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
-//import ImagePicker from 'react-native-image-picker';
+import React, { Component } from 'react';
+import api from '../services/api';
+import ImagePicker from 'react-native-image-picker';
 
 import { View, StyleSheet, TouchableOpacity, Text, TextInput, Image } from 'react-native'
 
@@ -9,6 +10,8 @@ export default class New extends Component {
     }
 
     state = {
+        preview: null,
+        image: null,
         author: '',
         place: '',
         description: '',
@@ -19,15 +22,58 @@ export default class New extends Component {
         ImagePicker.showImagePicker({
             title: 'Selecionar imagem',
         }, upload => {
+            if (upload.error) {
+                console.log('Error');
+            } else if (upload.didCancel) {
+                console.log('User canceled');
+            } else {
+                const preview = {
+                    uri: `data:image/jpeg;base64,${upload.data}`,
+                }
 
+                let prefix;
+                let ext;
+
+                if(upload.fileName) {
+                    [prefix, ext] = upload.fileName.split('.')
+                    ext = ext.toLowerCase() === 'heic' ? 'jpg' : ext;
+                } else {
+                    prefix = new Date().getTime();
+                    ext = 'jpg';
+                }
+
+                const image = {
+                    uri: upload.uri,
+                    type: upload.type,
+                    name: `${prefix}.${ext}`
+                };
+
+                this.setState({ preview, image });
+            }
         })
+    }
+
+    handleSubmit = async () => {
+        const data = new FormData();
+
+        data.append('image', this.state.image);
+        data.append('author', this.state.author);
+        data.append('place', this.state.place);
+        data.append('description', this.state.description);
+        data.append('hashtags', this.state.hashtags);
+
+        await api.post('posts', data)
+
+        this.props.navigation.navigate('Feed');
     }
 
     render() {
         return  <View style={styles.container}>
-            <TouchableOpacity style={styles.selectButton} /*onPress={this.handleSelectImage}*/>
+            <TouchableOpacity style={styles.selectButton} onPress={this.handleSelectImage} >
                 <Text style={styles.selectButtonText}>Selecionar imagem</Text>
             </TouchableOpacity>
+
+            { this.state.preview && <Image style={styles.preview} source={this.state.preview} /> }
 
             <TextInput
                 style={styles.input}
@@ -46,7 +92,7 @@ export default class New extends Component {
                 placeholder="Local da foto"
                 placeholderTextColor="#999"
                 value={this.state.place}
-                onChangeText={author => this.setState({ place })}
+                onChangeText={place => this.setState({ place })}
             />
 
             <TextInput
@@ -56,7 +102,7 @@ export default class New extends Component {
                 placeholder="Descrição"
                 placeholderTextColor="#999"
                 value={this.state.description}
-                onChangeText={author => this.setState({ description })}
+                onChangeText={description => this.setState({ description })}
             />
 
             <TextInput
@@ -66,10 +112,10 @@ export default class New extends Component {
                 placeholder="Hashtags"
                 placeholderTextColor="#999"
                 value={this.state.hashtags}
-                onChangeText={author => this.setState({ hashtags })}
+                onChangeText={hashtags => this.setState({ hashtags })}
             />
 
-            <TouchableOpacity style={styles.shareButton} onPress={() => {}}>
+            <TouchableOpacity style={styles.shareButton} onPress={this.handleSubmit}>
                 <Text style={styles.shareButtonText}>Compartilhar</Text>
             </TouchableOpacity>
         </View>;
