c901d435dc3c51f3b0b3e1b27ee1ce31ec578576
Mateus Leonhardt <mateus.leonhardt@conectt.com.br>
2019-09-04 11:27:47 -0300

    Added styled component

--- backend/src/config/upload.js
+++ backend/src/config/upload.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 const path = require('path');
 
 module.exports = {
-  storage: new multer.DiskStorage({
+  storage: multer.diskStorage({
     destination: path.resolve(__dirname, '..', '..', 'uploads'),
     filename(req, file, cb) {
       cb(null, file.originalname);
